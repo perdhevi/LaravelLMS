8b6e2a5488da022d3776e1f838e29a32a4786752
Raditya <raditya.perdhevi@gmail.com>
2017-02-28 20:19:34 +0700

    adding base model

--- database/migrations/2017_02_27_131302_create_course_table.php
+++ database/migrations/2017_02_27_131302_create_course_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCourseTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,14 +12,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        //
+        Schema::create('courses', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description');
+            $table->string('title');
+            $table->string('course_owner')->unique();
             $table->timestamps();
-            $table->timestamp('lastLogin');
         });
     }
 
@@ -30,6 +29,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        //
+        Schema::drop('courses');
     }
 }
